a3a16ef9bfa11b3383614b74f9fdbc2e3918f659
chiemelaumeh <chiemelaumeh1@gmail.com>
2024-01-18 21:58:44 -0600

    addOddNumbbersRecursively

--- algo1.py
+++ algo1.py
@@ -3,9 +3,21 @@
 
 # SOLUTION
 def factorial(n):
-    if n != 1:
+    while n != 1:
         return n * factorial(n-1)
     return n
 
 
-print(factorial(4))
+# print(factorial(4))
+
+
+# Write a recursive function sum_odd(list) that takes in a list of integers,
+# and returns the sum of only odd numbers inside the list.
+def addOddNumbers(list, index):
+    count = 0
+    while index < len(list):
+        if list[index] % 2 == 1:
+            count += list[index]
+        return count + addOddNumbers(list, index+1)
+    return count
+print(addOddNumbers([1,2,3,4,5,6,7,8,9,10],0))
