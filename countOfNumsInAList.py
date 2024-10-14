5bc8293027795af2fdf49eb993820fa9e8fd838e
chiemelaumeh <chiemelaumeh1@gmail.com>
2024-10-14 09:52:49 -0500

    countOfNumsInAList

---
+++ countOfNumsInAList.py
@@ -0,0 +1,22 @@
+# Algo to find the most occuring number is a list
+class Solution(object):
+    
+ def countOccurences(self, nums):
+    countDict = {}
+    for num in nums:
+        if num in countDict:
+            countDict[num]+=1
+        else:
+            countDict[num] = 1
+    # print(countDict)
+    highestOccuringNum = None
+    maxCount = 0
+    for num, count in countDict.items():
+        if count > maxCount:
+            maxCount = count
+            highestOccuringNum = num
+
+
+    return highestOccuringNum, maxCount
+callSolution = Solution()
+print(callSolution.countOccurences([1,2,3,4,5,2,4,6,4]))
